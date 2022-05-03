a2f7e83a0469d2a452068b11410c4aeef39cd79d
Антон Борисов <ivan-kruzenstern@yandex.ru>
2022-05-04 02:08:19 +0300

    hmmm

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import sys
 from os.path import abspath, dirname
+import os
 
 root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 pytest_plugins = [
     'tests.fixtures.fixture_data'
