08e92ad5e70f845a2f4a3ab4d8d4c5bcedfab9d3
Антон Борисов <ivan-kruzenstern@yandex.ru>
2022-05-06 21:22:52 +0300

    КуьÑRemoved counter variables and replaced it with lists. ReAdded func "check_list_of_homeworks". Removed all bot messages from public functions.Ð Keep it on main().

--- homework.py
+++ homework.py
@@ -37,9 +37,9 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 TIME_SIGNATURE_UNIX = 60 * 10
-RETRY_TIME = 10
-RETRY_TIME_AFTER_ERROR = 5
-# Добавил переменную с количеством секунд для ретрая после ошибки в main()
+RETRY_TIME = 60 * 10
+RETRY_TIME_AFTER_ERROR = 60
+
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -50,16 +50,6 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-try:
-    bot_for_sendings_error = telegram.Bot(token=TELEGRAM_TOKEN)
-except telegram.error.InvalidToken:
-    logger.error('Telegram token is invalid. Check this carefully!'
-                 'Bot will not be able to send an error messages!')
-
-API_ERROR_MESSAGE_COUNT = 0
-CHECK_RESPONSE_ERROR_MESSAGE_COUNT = 0
-CHECK_RESPONSE_ISINSTANCE_COUNT = 0
-
 
 def send_message(bot: telegram.Bot, message: str) -> None:
     """Bot message sender."""
@@ -68,9 +58,11 @@ def send_message(bot: telegram.Bot, message: str) -> None:
             chat_id=TELEGRAM_CHAT_ID,
             text=message
         )
-        logger.info('Message is sent successfully!')
-    except telegram.TelegramError as e:
-        logger.error(f'Message cannot be sent: {e}')
+        info_message = 'Message is sent successfully!'
+        logger.info(info_message)
+    except telegram.TelegramError:
+        error_message = 'Message cannot be sent.'
+        logger.error(error_message)
 
 
 def get_api_answer(current_timestamp: int) -> dict:
@@ -84,29 +76,15 @@ def get_api_answer(current_timestamp: int) -> dict:
             params=params
         )
     except requests.exceptions.ConnectionError:
-        message = 'Endpoint is unreachable. Try another url.'
-        logger.error(message)
-        send_message(
-            bot_for_sendings_error,
-            message
-        )
+        error_message = 'Endpoint is unreachable. Try another url.'
+        logger.error(error_message)
         raise ConnectionError
     if response.status_code != HTTPStatus.OK:
-        global API_ERROR_MESSAGE_COUNT
-        API_ERROR_MESSAGE_COUNT += 1
         logger.error(
             f'Cannot access to api. '
             f'Status code: {response.status_code}'
         )
-        # Отправляем сообщение в телеграм только когда счётчик  == 1
-        #  При этом логгируем каждое такое событие
-        if API_ERROR_MESSAGE_COUNT == 1:
-            send_message(
-                bot_for_sendings_error,
-                message='Error message: Cannot access to api.'
-            )
         raise response.raise_for_status()
-    API_ERROR_MESSAGE_COUNT = 0
     try:
         return response.json()
     except JSONDecodeError as error:
@@ -118,35 +96,22 @@ def get_api_answer(current_timestamp: int) -> dict:
 
 def check_response(response: dict) -> list:
     """Checks if api answer is correct."""
-    if response is None:
-        logger.error('API answer is not a valid.')
-        raise TypeError
+    if not isinstance(response, dict):
+        error_message = 'API answer is not a valid.'
+        logger.error(error_message)
+        raise TypeError(error_message)
     try:
-        response['homeworks']
-    except KeyError as e:
-        global CHECK_RESPONSE_ERROR_MESSAGE_COUNT
-        CHECK_RESPONSE_ERROR_MESSAGE_COUNT += 1
-        logger.error(f'There is no key {e}')
-        if CHECK_RESPONSE_ERROR_MESSAGE_COUNT == 1:
-            send_message(
-                bot=bot_for_sendings_error,
-                message='Cannot find you homeworks while parsing api :('
-            )
-        raise KeyError
-    lst_of_homeworks = response['homeworks']
+        lst_of_homeworks = response['homeworks']
+    except KeyError:
+        error_message = (
+            'Somehow there is no key named "homeworks" in the data.'
+        )
+        logger.error(error_message)
+        raise KeyError(error_message)
     if not isinstance(lst_of_homeworks, list):
-        global CHECK_RESPONSE_ISINSTANCE_COUNT
-        CHECK_RESPONSE_ISINSTANCE_COUNT += 1
         message = 'List of homeworks is not actually a list!'
         logger.error(message)
-        if CHECK_RESPONSE_ISINSTANCE_COUNT == 1:
-            send_message(
-                bot=bot_for_sendings_error,
-                message='Could not get a list of your homeworks'
-            )
         raise TypeError(message)
-    CHECK_RESPONSE_ISINSTANCE_COUNT = 0
-    CHECK_RESPONSE_ERROR_MESSAGE_COUNT = 0
     return lst_of_homeworks
 
 
@@ -156,26 +121,38 @@ def parse_status(homework: dict) -> str:
     if it is clearly found.
     """
     try:
-        homework['homework_name']
-    except KeyError as e:
-        logger.error(
-            f'Cannot access to homework_name via '
-            f'"homework_name" key: {e}'
+        homework_name = homework['homework_name']
+    except KeyError:
+        error_message = (
+            'Cannot access to homework_name via key word "homework_name".'
         )
+        logger.error(error_message)
+        raise KeyError(error_message)
     try:
-        homework['status']
-    except KeyError as e:
-        logger.error(
-            f'Cannot access to status via '
-            f'"status" key: {e}'
+        homework_status = homework['status']
+    except KeyError:
+        error_message = (
+            'Cannot access to status via key word "status".'
         )
-    if homework['status'] not in HOMEWORK_STATUSES:
-        logger.error('There is not valid value.')
-        raise KeyError
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
+        logger.error(error_message)
+        raise KeyError(error_message)
+    if homework_status not in HOMEWORK_STATUSES:
+        error_message = 'There is not valid value of homework status.'
+        logger.error(error_message)
+        raise KeyError(error_message)
     verdict = HOMEWORK_STATUSES.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+
+
+def check_list_of_homeworks(homeworks: list) -> bool:
+    """Function that inspects if list of homeworks empty or not."""
+    if not isinstance(homeworks, list):
+        error_message = 'Function expects list as an argument.'
+        logger.error(error_message)
+        raise TypeError(error_message)
+    if len(homeworks) > 0:
+        return True
+    return False
 
 
 def check_tokens() -> bool:
@@ -187,41 +164,42 @@ def check_tokens() -> bool:
 
 def main() -> None:
     """The bot's main logic."""
+    homeworks = []
+    errors = []
+    start_message = 'Searching for updates...'
     token_error_name = (
         'Some tokens or all of them are missed! '
         'Check that you have specified tokens and retry!'
     )
-    bot = None
+    current_timestamp = int(time.time())
     if not check_tokens():
         logger.critical(
             msg=token_error_name
         )
-        raise KeyError('Required environment variable is missed!')
+        raise KeyError(token_error_name)
     try:
         bot = telegram.Bot(token=TELEGRAM_TOKEN)
     except telegram.error.InvalidToken:
         logger.error('Telegram token is invalid!')
-    current_timestamp = int(time.time())
-    no_update_count = 0
-    body_error_count = 0
+        raise telegram.error.InvalidToken
+    send_message(bot, start_message)
     while True:
         try:
             response = get_api_answer(current_timestamp - TIME_SIGNATURE_UNIX)
-            lst_of_update = check_response(response)
-            if len(lst_of_update) > 0:
-                no_update_count = 0
-                send_message(bot, parse_status(lst_of_update[0]))
-            else:
-                no_update_count += 1
-                if no_update_count == 1:
-                    send_message(bot, 'There is no updates yet!')
-            body_error_count = 0
+            lst_of_homeworks = check_response(response)
+            list_bool = check_list_of_homeworks(lst_of_homeworks)
+            if list_bool:
+                status = parse_status(lst_of_homeworks[0])
+                if status not in homeworks:
+                    homeworks.append(status)
+                    send_message(bot, status)
+            errors.clear()
             time.sleep(RETRY_TIME)
-        except Exception:
-            body_error_count += 1
-            message = 'Programm failure!'
+        except Exception as e:
+            message = f'Programm failure! \n {e}'
             logger.error(message)
-            if body_error_count == 1:
+            if e.__repr__() not in errors:
+                errors.append(e.__repr__())
                 send_message(bot, message)
             time.sleep(RETRY_TIME_AFTER_ERROR)
 
